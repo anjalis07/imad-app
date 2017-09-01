bf27195f2f6e82a459cf6a838617e37a1e096e23
anjalis07 <30894717+anjalis07@users.noreply.github.com>
2017-09-01 16:45:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,10 +45,10 @@ app.get('/article-three', function(req,res){
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+//var port = 80;
+//app.listen(port, function () {
+//  console.log(`IMAD course app listening on port ${port}!`);
+//});
 
 // DATABASE CONNECTIVITY
 var pool=new Pool(config);
